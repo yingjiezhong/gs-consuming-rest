313717c8933d16a38ce953ac5077338aa9e7afaa
Yingjie <xining.zhong@gmail.com>
2019-04-04 07:47:58 -0400

    javascript is able to receive SseEmitter.

--- complete/src/main/js/sseClient.js
+++ complete/src/main/js/sseClient.js
@@ -1,5 +1,34 @@
 var sse = new EventSource('http://localhost:8080/job/sseInResponseEntity');
 alert("created sse object")
+
+sse.onopen = function() {
+    console.log("connection open")
+}
+
+sse.onclose = function() {
+    console.log("connection close")
+}
+
+sse.addEventListener('connections', updateConnections, false);
+
+function updateConnections(evt) {
+console.log("update connections: " + evt.data)
+}
+
+sse.addEventListener('progress', updateProgress, false);
+
+function updateProgress(evt) {
+console.log("update Progress: " + evt.data)
+}
+
+sse.addEventListener('complete', updateComplete, false);
+
+function updateComplete(evt) {
+console.log("update Complete: " + evt.data)
+sse.close();
+}
+
+
 sse.onmessage = function (evt) {
     console.log("received event: " + evt.data)
 //    var el = document.getElementById('sseInResponseEntity');
